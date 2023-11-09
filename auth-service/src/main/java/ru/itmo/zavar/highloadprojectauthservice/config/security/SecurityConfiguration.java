94299b8a62707b053460c2d3e73269de332ed3a8
gramdel <legovec96@gmail.com>
2023-11-10 02:25:01 +0300

    Added FeignClient for accessing UserService to AuthService

--- auth-service/src/main/java/ru/itmo/zavar/highloadprojectauthservice/config/security/SecurityConfiguration.java
+++ auth-service/src/main/java/ru/itmo/zavar/highloadprojectauthservice/config/security/SecurityConfiguration.java
@@ -1,28 +1,31 @@
-package ru.itmo.zavar.highloadprojectauthservice.security.config;
+package ru.itmo.zavar.highloadprojectauthservice.config.security;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
-import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
-import ru.itmo.zavar.highloadprojectauthservice.service.UserService;
+import ru.itmo.zavar.highloadprojectauthservice.client.UserServiceClient;
+import ru.itmo.zavar.highloadprojectauthservice.dto.inner.response.UserEntityResponse;
+import ru.itmo.zavar.highloadprojectauthservice.mapper.UserEntityMapper;
 
 @Configuration
 @EnableWebSecurity
-@EnableMethodSecurity()
 @RequiredArgsConstructor
 public class SecurityConfiguration {
-    private final UserService userService;
+    private final UserServiceClient userServiceClient;
+    private final UserEntityMapper userEntityMapper;
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
@@ -41,7 +44,13 @@ public class SecurityConfiguration {
     @Bean
     public AuthenticationProvider authenticationProvider() {
         DaoAuthenticationProvider authProvider = new DaoAuthenticationProvider();
-        authProvider.setUserDetailsService(userService.userDetailsService());
+        authProvider.setUserDetailsService(username -> {
+            ResponseEntity<UserEntityResponse> response = userServiceClient.getByUsername(username);
+            if (response.getStatusCode().isError()) {
+                throw new UsernameNotFoundException("User not found");
+            }
+            return userEntityMapper.fromResponse(response.getBody());
+        });
         authProvider.setPasswordEncoder(passwordEncoder());
         return authProvider;
     }

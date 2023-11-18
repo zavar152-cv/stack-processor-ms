1649853e7a22eaecce3d1731fd69ac37614e98ce
gramdel <legovec96@gmail.com>
2023-11-18 14:15:30 +0300

    Renamed package on request-service; renamed DTO; moved business-logic from controller-level to service-level; fixed mappings of controller's methods according to REST conventions.

--- request-service/src/main/java/ru/itmo/zavar/highloadprojectrequestservice/dto/inner/request/RequestServiceRequest.java
+++
@@ -1,9 +0,0 @@
-package ru.itmo.zavar.highloadprojectrequestservice.dto.inner.request;
-
-import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.NotNull;
-import lombok.Builder;
-
-@Builder
-public record RequestServiceRequest(@NotBlank String text, @NotNull Boolean debug) {
-}

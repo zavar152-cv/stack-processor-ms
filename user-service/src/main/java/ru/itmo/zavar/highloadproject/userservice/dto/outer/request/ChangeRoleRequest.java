35be78a804a0af6894d6ce07b85aefd36e76c70e
gramdel <legovec96@gmail.com>
2023-11-18 22:37:55 +0300

    Moved business-logic from controller-level to service-level on user-service. Fixed mappings of controller's methods according to REST conventions; changed paths on gateway and auth-service accordingly. Deleted getById (wasn't used anywhere); added new method for getting requests.

--- user-service/src/main/java/ru/itmo/zavar/highloadproject/userservice/dto/outer/request/ChangeRoleRequest.java
+++ user-service/src/main/java/ru/itmo/zavar/highloadproject/userservice/dto/outer/request/ChangeRoleRequest.java
@@ -1,9 +1,8 @@
 package ru.itmo.zavar.highloadproject.userservice.dto.outer.request;
 
 import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.Size;
 import lombok.Builder;
 
 @Builder
-public record ChangeRoleRequest(@NotBlank @Size(min = 5, max = 25) String username, @NotBlank String role) {
+public record ChangeRoleRequest(@NotBlank String role) {
 }

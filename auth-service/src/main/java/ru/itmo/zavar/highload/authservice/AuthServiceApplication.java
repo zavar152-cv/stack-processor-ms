90b6354ba6d63ae8008a94e21c61c44b438a75a3
gramdel <legovec96@gmail.com>
2023-11-26 03:38:54 +0300

    Added circuit breaker to auth-service.

--- auth-service/src/main/java/ru/itmo/zavar/highload/authservice/AuthServiceApplication.java
+++ auth-service/src/main/java/ru/itmo/zavar/highload/authservice/AuthServiceApplication.java
@@ -1,15 +1,42 @@
 package ru.itmo.zavar.highload.authservice;
 
+import io.github.resilience4j.circuitbreaker.CircuitBreaker;
+import io.github.resilience4j.core.registry.EntryAddedEvent;
+import io.github.resilience4j.core.registry.EntryRemovedEvent;
+import io.github.resilience4j.core.registry.EntryReplacedEvent;
+import io.github.resilience4j.core.registry.RegistryEventConsumer;
+import lombok.NonNull;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.context.annotation.Bean;
 
+@Slf4j
 @EnableFeignClients
 @SpringBootApplication
 public class AuthServiceApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(AuthServiceApplication.class, args);
+    }
+
+    @Bean
+    public RegistryEventConsumer<CircuitBreaker> myCircuitBreakerRegistryEventConsumer() {
+        return new RegistryEventConsumer<>() {
+            @Override
+            public void onEntryAddedEvent(@NonNull EntryAddedEvent<CircuitBreaker> entryAddedEvent) {
+                entryAddedEvent.getAddedEntry().getEventPublisher().onStateTransition(event -> log.info(event.toString()));
+            }
+
+            @Override
+            public void onEntryRemovedEvent(@NonNull EntryRemovedEvent<CircuitBreaker> entryRemoveEvent) {
+            }
+
+            @Override
+            public void onEntryReplacedEvent(@NonNull EntryReplacedEvent<CircuitBreaker> entryReplacedEvent) {
+            }
+        };
     }
 
 }

77a8e59c01f4017ab80f94aa1053c0952cc18b93
gramdel <legovec96@gmail.com>
2023-11-26 13:40:34 +0300

    Removed fallback method (was useless anyways); improved CB configuration and error handling on auth-service

--- auth-service/src/main/java/ru/itmo/zavar/highload/authservice/client/UserServiceClient.java
+++ auth-service/src/main/java/ru/itmo/zavar/highload/authservice/client/UserServiceClient.java
@@ -2,24 +2,15 @@ package ru.itmo.zavar.highload.authservice.client;
 
 import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
 import org.springframework.cloud.openfeign.FeignClient;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.server.ResponseStatusException;
 import ru.itmo.zavar.highload.authservice.config.feign.FeignConfiguration;
 import ru.itmo.zavar.highload.authservice.dto.inner.UserDTO;
 
 @FeignClient(name = "user-service", path = "${server.servlet.context-path}", configuration = FeignConfiguration.class)
 public interface UserServiceClient {
     @GetMapping("/users/{username}")
-    @CircuitBreaker(name = "UserServiceClientCB", fallbackMethod = "fallback")
+    @CircuitBreaker(name = "UserServiceClientCB")
     ResponseEntity<UserDTO> getUser(@PathVariable String username);
-
-    default ResponseEntity<UserDTO> fallback(String username, Exception e) {
-        if (e instanceof ResponseStatusException ex && ex.getStatusCode() == HttpStatus.NOT_FOUND) {
-            throw ex;
-        }
-        throw new ResponseStatusException(HttpStatus.SERVICE_UNAVAILABLE, "Can't get user from user-service");
-    }
 }

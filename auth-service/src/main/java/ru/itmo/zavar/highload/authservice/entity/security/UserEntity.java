1346f3b18fd9da825a22711116d6d136437901e4
gramdel <legovec96@gmail.com>
2023-11-27 22:07:07 +0300

    Removed spring-data-jpa from auth-service (wasn't used); fixed method signIn (no extra request to user-service now). Added default values to configs of all services to connect to db (if not in docker).

--- auth-service/src/main/java/ru/itmo/zavar/highload/authservice/entity/security/UserEntity.java
+++ auth-service/src/main/java/ru/itmo/zavar/highload/authservice/entity/security/UserEntity.java
@@ -1,6 +1,5 @@
 package ru.itmo.zavar.highload.authservice.entity.security;
 
-import jakarta.persistence.*;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
@@ -18,34 +17,20 @@ import java.util.Collection;
 import java.util.List;
 import java.util.Set;
 
-
 @Data
 @Builder
 @NoArgsConstructor
 @AllArgsConstructor
-@Entity
-@Table(name = "users")
 public final class UserEntity implements UserDetails {
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     @NotBlank
     @Size(min = 5, max = 25)
     private String username;
     @NotBlank
     private String password;
-    @ManyToMany(fetch = FetchType.EAGER)
-    @JoinTable(
-            name = "users_roles",
-            joinColumns = @JoinColumn(name = "user_entity_id"),
-            inverseJoinColumns = @JoinColumn(name = "roles_id"))
     @NotNull
     private Set<RoleEntity> roles;
 
-    @ManyToMany(fetch = FetchType.EAGER)
-    @JoinTable(name = "users_requests",
-            joinColumns = @JoinColumn(name = "user_id"),
-            inverseJoinColumns = @JoinColumn(name = "requests_id"))
     private List<RequestEntity> requests;
 
     @Override

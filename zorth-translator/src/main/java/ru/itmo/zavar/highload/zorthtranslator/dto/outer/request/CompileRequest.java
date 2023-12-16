3648a4f7c4e982863a4cda76c345168731f816ad
gramdel <legovec96@gmail.com>
2023-12-17 00:38:03 +0300

    Added specification for zorth-translator-service (should deal with methods returning "Page" tho). Also, fixed user-service and processor-service a bit.

--- zorth-translator/src/main/java/ru/itmo/zavar/highload/zorthtranslator/dto/outer/request/CompileRequest.java
+++ zorth-translator/src/main/java/ru/itmo/zavar/highload/zorthtranslator/dto/outer/request/CompileRequest.java
@@ -1,9 +1,11 @@
 package ru.itmo.zavar.highload.zorthtranslator.dto.outer.request;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import lombok.Builder;
 
 @Builder
-public record CompileRequest(@NotNull Boolean debug, @NotBlank String text) {
+public record CompileRequest(@Schema(example = "true") @NotNull Boolean debug,
+                             @Schema(example = "variable a\n3 a !\na @") @NotBlank String text) {
 }

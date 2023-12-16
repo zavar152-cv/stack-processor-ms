dad4ad0f42122b0f90b707d9d4dc1fc90828e5d6
gramdel <legovec96@gmail.com>
2023-12-16 17:47:30 +0300

    Added specification for user-service; small naming fix in application.yml for tests on processor

--- user-service/src/main/java/ru/itmo/zavar/highload/userservice/dto/outer/request/ChangeRoleRequest.java
+++ user-service/src/main/java/ru/itmo/zavar/highload/userservice/dto/outer/request/ChangeRoleRequest.java
@@ -1,8 +1,9 @@
 package ru.itmo.zavar.highload.userservice.dto.outer.request;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.NotBlank;
 import lombok.Builder;
 
 @Builder
-public record ChangeRoleRequest(@NotBlank String role) {
+public record ChangeRoleRequest(@Schema(example = "ROLE_VIP") @NotBlank String role) {
 }

5f2504183022a1eac480613d702eaad31ea9f763
gramdel <legovec96@gmail.com>
2023-12-16 18:23:10 +0300

    Added specification for auth-service

--- auth-service/src/main/java/ru/itmo/zavar/highload/authservice/dto/outer/response/SignInResponse.java
+++ auth-service/src/main/java/ru/itmo/zavar/highload/authservice/dto/outer/response/SignInResponse.java
@@ -1,7 +1,9 @@
 package ru.itmo.zavar.highload.authservice.dto.outer.response;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Builder;
 
 @Builder
-public record SignInResponse(String token) {
+public record SignInResponse(
+        @Schema(example = "eyJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ0ZXN0IiwiaWF0IjoxNzAxMTIxOTI0LCJleHAiOjE3MDExMjMzNjR9.TEA-S9YEk53iFg1GQL1_pimcoXciroMdLEZJBu7GSAo") String token) {
 }

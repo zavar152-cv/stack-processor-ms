7b6307d1ed756308a41f4ffc96bbf0e1ebeca83c
GramDEL <legovec96@gmail.com>
2023-12-18 21:43:41 +0300

    Added specification to file-service

--- file-service/src/main/java/ru/itmo/zavar/highload/fileservice/controller/FileController.java
+++ file-service/src/main/java/ru/itmo/zavar/highload/fileservice/controller/FileController.java
@@ -1,5 +1,11 @@
 package ru.itmo.zavar.highload.fileservice.controller;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.ArraySchema;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Positive;
 import lombok.RequiredArgsConstructor;
@@ -17,6 +23,7 @@ import ru.itmo.zavar.highload.fileservice.dto.response.FileInfoResponse;
 import ru.itmo.zavar.highload.fileservice.exception.StorageException;
 import ru.itmo.zavar.highload.fileservice.service.StorageService;
 import ru.itmo.zavar.highload.fileservice.util.RoleConstants;
+import ru.itmo.zavar.highload.fileservice.util.SpringWebErrorModel;
 
 import java.util.List;
 import java.util.NoSuchElementException;
@@ -26,13 +33,61 @@ import java.util.NoSuchElementException;
 public class FileController {
     private final StorageService storageService;
 
+    @Operation(
+            summary = "Get all files",
+            description = "This method finds information about all files (if called by administrator)."
+    )
+    @ApiResponses({
+            @ApiResponse(responseCode = "200", description = "Information about files was successfully obtained",
+                    content = {@Content(
+                            mediaType = "application/json",
+                            array = @ArraySchema(schema = @Schema(implementation = FileInfoResponse.class))
+                    )}
+            ),
+            @ApiResponse(responseCode = "403", description = "Forbidden: only administrators can use this method",
+                    content = {@Content(
+                            mediaType = "application/json",
+                            schema = @Schema(implementation = SpringWebErrorModel.class)
+                    )}
+            )
+    })
     @GetMapping("/files")
     @PreAuthorize("hasRole('" + RoleConstants.ADMIN + "')")
     public ResponseEntity<List<FileInfoResponse>> listUploadedFiles() {
         return ResponseEntity.ok(storageService.listAll());
     }
 
-    @GetMapping("/files/info/{id}")
+    @Operation(
+            summary = "Get information about file",
+            description = "This method finds information about file by its id (if file belongs to the caller)."
+    )
+    @ApiResponses({
+            @ApiResponse(responseCode = "200", description = "Information about file was successfully obtained",
+                    content = {@Content(
+                            mediaType = "application/json",
+                            schema = @Schema(implementation = FileInfoResponse.class)
+                    )}
+            ),
+            @ApiResponse(responseCode = "400", description = "Bad request: invalid path variable",
+                    content = {@Content(
+                            mediaType = "application/json",
+                            schema = @Schema(implementation = SpringWebErrorModel.class)
+                    )}
+            ),
+            @ApiResponse(responseCode = "403", description = "Forbidden: file doesn't belong to the caller",
+                    content = {@Content(
+                            mediaType = "application/json",
+                            schema = @Schema(implementation = SpringWebErrorModel.class)
+                    )}
+            ),
+            @ApiResponse(responseCode = "404", description = "File doesn't exist",
+                    content = {@Content(
+                            mediaType = "application/json",
+                            schema = @Schema(implementation = SpringWebErrorModel.class)
+                    )}
+            )
+    })
+    @GetMapping("/files/{id}/info")
     @PreAuthorize("@databaseStorageService.isOwnedByUser(authentication.name, #id)")
     public ResponseEntity<FileInfoResponse> getFileInfoById(@PathVariable @Positive @NotNull Long id) {
         try {
@@ -43,7 +98,37 @@ public class FileController {
         }
     }
 
-    @GetMapping("/files/content/{id}")
+    @Operation(
+            summary = "Get content of file",
+            description = "This method finds content of file by its id (if file belongs to the caller)."
+    )
+    @ApiResponses({
+            @ApiResponse(responseCode = "200", description = "Content of file was successfully obtained",
+                    content = {@Content(
+                            mediaType = "application/json",
+                            schema = @Schema(implementation = FileContentResponse.class)
+                    )}
+            ),
+            @ApiResponse(responseCode = "400", description = "Bad request: invalid path variable",
+                    content = {@Content(
+                            mediaType = "application/json",
+                            schema = @Schema(implementation = SpringWebErrorModel.class)
+                    )}
+            ),
+            @ApiResponse(responseCode = "403", description = "Forbidden: file doesn't belong to the caller",
+                    content = {@Content(
+                            mediaType = "application/json",
+                            schema = @Schema(implementation = SpringWebErrorModel.class)
+                    )}
+            ),
+            @ApiResponse(responseCode = "404", description = "File doesn't exist",
+                    content = {@Content(
+                            mediaType = "application/json",
+                            schema = @Schema(implementation = SpringWebErrorModel.class)
+                    )}
+            )
+    })
+    @GetMapping("/files/{id}/content")
     @PreAuthorize("@databaseStorageService.isOwnedByUser(authentication.name, #id)")
     public ResponseEntity<FileContentResponse> getFileContentById(@PathVariable @Positive @NotNull Long id) {
         try {
@@ -54,7 +139,33 @@ public class FileController {
         }
     }
 
-
+    @Operation(
+            summary = "Download file",
+            description = "This method is for downloading file by its name (if file belongs to caller)."
+    )
+    @ApiResponses({
+            @ApiResponse(responseCode = "200", description = "File was successfully obtained",
+                    content = @Content(mediaType = "application/octet-stream")
+            ),
+            @ApiResponse(responseCode = "400", description = "Bad request: invalid path variable",
+                    content = {@Content(
+                            mediaType = "application/json",
+                            schema = @Schema(implementation = SpringWebErrorModel.class)
+                    )}
+            ),
+            @ApiResponse(responseCode = "403", description = "Forbidden: file doesn't belong to the caller",
+                    content = {@Content(
+                            mediaType = "application/json",
+                            schema = @Schema(implementation = SpringWebErrorModel.class)
+                    )}
+            ),
+            @ApiResponse(responseCode = "404", description = "File doesn't exist",
+                    content = {@Content(
+                            mediaType = "application/json",
+                            schema = @Schema(implementation = SpringWebErrorModel.class)
+                    )}
+            )
+    })
     @GetMapping("/download/{filename:.+}")
     @ResponseBody
     @PreAuthorize("@databaseStorageService.isOwnedByUser(authentication.name, #filename)")
@@ -68,6 +179,25 @@ public class FileController {
         }
     }
 
+    @Operation(
+            summary = "Upload file",
+            description = "This method uploads the file (if there is no conflict)."
+    )
+    @ApiResponses({
+            @ApiResponse(responseCode = "200", description = "File was successfully uploaded", content = @Content),
+            @ApiResponse(responseCode = "400", description = "Bad request: invalid request parameter",
+                    content = {@Content(
+                            mediaType = "application/json",
+                            schema = @Schema(implementation = SpringWebErrorModel.class)
+                    )}
+            ),
+            @ApiResponse(responseCode = "409", description = "Conflict: file is empty or has already been uploaded",
+                    content = {@Content(
+                            mediaType = "application/json",
+                            schema = @Schema(implementation = SpringWebErrorModel.class)
+                    )}
+            )
+    })
     @PostMapping("/upload")
     public ResponseEntity<?> handleFileUpload(@RequestParam("file") MultipartFile file, Authentication authentication) {
         try {
--- file-service/src/main/java/ru/itmo/zavar/highload/fileservice/dto/response/FileInfoResponse.java
+++ file-service/src/main/java/ru/itmo/zavar/highload/fileservice/dto/response/FileInfoResponse.java
@@ -1,8 +1,9 @@
 package ru.itmo.zavar.highload.fileservice.dto.response;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Builder;
 
 @Builder
-public record FileInfoResponse(Long id, String name, String ownerName) {
-
+public record FileInfoResponse(@Schema(example = "1") Long id, @Schema(example = "input.zorth") String name,
+                               @Schema(example = "admin") String ownerName) {
 }

888f75a4fa6e3c39c0bbbb375ea03ced6ef6d166
GramDEL <legovec96@gmail.com>
2023-12-18 21:43:41 +0300

    Added specification to file-service

--- file-service/src/main/java/ru/itmo/zavar/highload/fileservice/dto/response/FileContentResponse.java
+++ file-service/src/main/java/ru/itmo/zavar/highload/fileservice/dto/response/FileContentResponse.java
@@ -1,8 +1,9 @@
 package ru.itmo.zavar.highload.fileservice.dto.response;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Builder;
 
 @Builder
-public record FileContentResponse(Long id, String name, String content) {
-
+public record FileContentResponse(@Schema(example = "1") Long id, @Schema(example = "input.zorth") String name,
+                                  @Schema(example = "variable a\n3 a !\na @") String content) {
 }

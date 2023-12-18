fe830fd8270323790afaa23bbbfa9dc617dd3919
Zavar30 <yarusking@mail.ru>
2023-12-18 15:13:06 +0300

    add notification-service via email

---
+++ notification-service/src/main/java/ru/itmo/zavar/highload/notificationservice/service/impl/NotificationSenderImpl.java
@@ -0,0 +1,29 @@
+package ru.itmo.zavar.highload.notificationservice.service.impl;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.stereotype.Component;
+import ru.itmo.zavar.highload.notificationservice.dto.NotificationRequest;
+import ru.itmo.zavar.highload.notificationservice.service.NotificationSender;
+
+@Component
+@RequiredArgsConstructor
+@Slf4j
+public class NotificationSenderImpl implements NotificationSender {
+
+    private final JavaMailSender emailSender;
+
+    @KafkaListener(topics = "notification", groupId = "mail")
+    @Override
+    public void sendSimpleMessage(NotificationRequest notificationRequest) {
+        SimpleMailMessage message = new SimpleMailMessage();
+        message.setFrom("noreply@highload.ru");
+        message.setTo(notificationRequest.to());
+        message.setSubject(notificationRequest.subject());
+        message.setText(notificationRequest.text());
+        emailSender.send(message);
+    }
+}
